fe6b48b239baca589031be8d92e9469d916a0f81
Alexander Beniaminov <abeniaminov@gmail.com>
2018-02-22 21:53:47 +0300

    fix minor errors

--- src/poolets_server.erl
+++ src/poolets_server.erl
@@ -41,24 +41,24 @@ Name :: atom()  | pid() |
 -type start_ret() :: {'ok', pid()} | 'ignore' | {'error', term()}.
 
 -record(state, {
-    workers  = [] :: [pid()],
+    workers = [] :: [pid()],
     overflow = 0 :: non_neg_integer(),
     shutdown = 1000 :: non_neg_integer() | brutal_kill,
     enabled = false
 }).
 
 
--spec demand(Pool :: pool()) -> pid() | full.
+-spec demand(Pool :: pool()) -> pid() | full | disabled | timeout.
 demand(Pool) ->
-    demand(Pool,  ?TIMEOUT).
+    demand(Pool, ?TIMEOUT).
 
 
 
 -spec demand(Pool :: pool(), Timeout :: timeout())
         -> pid() | full | disabled | timeout.
 demand(Pool, Timeout) ->
+    TRef = erlang:make_ref(),
     try
-        TRef = erlang:make_ref(),
         gen_server:call(Pool, {demand, TRef}, Timeout)
     catch
         _Class:_Reason ->
@@ -82,7 +82,7 @@ transaction(Pool, Fun) ->
     Timeout :: timeout()) -> any().
 transaction(Pool, Fun, Timeout) ->
     case demand(Pool, Timeout) of
-        {worker, Worker}  ->
+        {worker, Worker} ->
             try
                 Fun(Worker)
             after
@@ -93,16 +93,16 @@ transaction(Pool, Fun, Timeout) ->
 
 
 -spec start(PoolArgs :: proplists:proplist(),
-    WorkerArgs:: proplists:proplist())
+    WorkerArgs :: proplists:proplist())
         -> start_ret().
 start(PoolArgs, WorkerArgs) ->
     start_pool(start, PoolArgs, WorkerArgs).
 
 
 -spec start_link(PoolArgs :: proplists:proplist(),
-    WorkerArgs:: proplists:proplist())
+    WorkerArgs :: proplists:proplist())
         -> start_ret().
-start_link(PoolArgs, WorkerArgs)  ->
+start_link(PoolArgs, WorkerArgs) ->
     start_pool(start_link, PoolArgs, WorkerArgs).
 
 -spec stop(Pool :: pool()) -> ok.
@@ -117,31 +117,34 @@ init({PoolArgs, WorkerArgs}) ->
     init_ets(working),
     init_ets(waiting),
     init_ets(launching),
-    init(PoolArgs,  #state{}).
+    init(PoolArgs, #state{}).
 
-init([{name, Pool} | Rest],  State)  ->
+init([{name, Pool} | Rest], State) ->
     erlang:put(pool_name, Pool),
     init(Rest, State);
-init([{mfa, MFA} | Rest],  State) when is_tuple(MFA) ->
+init([{mfa, MFA} | Rest], State) when is_tuple(MFA) ->
     erlang:put(mfa, MFA),
     init(Rest, State);
-init([{init_size, Size} | Rest],  State) when is_integer(Size) ->
+init([{init_size, Size} | Rest], State) when is_integer(Size) ->
     erlang:put(init_size, Size),
-    init(Rest,  State);
-init([{max_overflow, MaxOverflow} | Rest],  State) when is_integer(MaxOverflow) ->
+    init(Rest, State);
+init([{max_overflow, MaxOverflow} | Rest], State) when is_integer(MaxOverflow) ->
     erlang:put(max_overflov, MaxOverflow),
-    init(Rest,  State);
-init([_ | Rest],  State) ->
-    init(Rest,  State);
-init([],  State) ->
+    init(Rest, State);
+init([_ | Rest], State) ->
+    init(Rest, State);
+init([], State) ->
     NewWorkers = prepopulate(),
     {ok, State#state{workers = NewWorkers}}.
 
-handle_call({demand, TRef}, {FromPid, _Tag} = From, state#{enabled = true} = State) ->
+handle_call(
+    {demand, TRef},
+    {FromPid, _Tag} = From,
+    #state{enabled = true} = State) ->
     CRef = erlang:monitor(process, FromPid),
     R = try_get(State#state.overflow, TRef, CRef, From),
     case R of
-        {worker, Worker}  ->
+        {worker, Worker} ->
             {reply, {worker, Worker}, State};
         {wait, CRef} ->
             {noreply, State};
@@ -152,7 +155,7 @@ handle_call({demand, TRef}, {FromPid, _Tag} = From, state#{enabled = true} = Sta
 
 
 
-handle_call({demand, _TRef}, _From, state#{enabled = false} = State) ->
+handle_call({demand, _TRef}, _From, #state{enabled = false} = State) ->
     {reply, {fault, disabled}, State};
 
 
@@ -188,13 +191,11 @@ handle_cast(_Msg, State) ->
     {noreply, State}.
 
 
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% delayed part of start workers
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-handle_info({new_worker,  LauncherPid, {ok, Worker}}, #state{workers = Workers, enabled = IsEnabled} = State) ->
+handle_info({new_worker, LauncherPid, {ok, Worker}}, #state{workers = Workers, enabled = IsEnabled} = State) ->
     link(Worker),
     TLaunching = ?TLaunching,
     InitSize = erlang:get(init_size),
@@ -204,15 +205,15 @@ handle_info({new_worker,  LauncherPid, {ok, Worker}}, #state{workers = Workers, 
             give_back(Worker, IsEnabled);
         [] -> continue
     end,
-    {noreply, State#state{workers = [Worker | Workers], enabled =  (length(Workers) > InitSize-1) }};
+    {noreply, State#state{workers = [Worker | Workers], enabled = (length(Workers) > InitSize - 1)}};
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% client unexpected dead
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 handle_info({'DOWN', CRef, _, _, _}, #state{workers = Workers} = State) ->
     true = ets:match_delete(?TWaiting, {'$1', CRef, '$2', '_'}),
-    true = ets:match_delete(?TLaunching, {'$1', '$2', '_' }),
-    case ets:match(?TWorking, {'$1', '_' , CRef, '_' }) of
+    true = ets:match_delete(?TLaunching, {'$1', '$2', '_'}),
+    case ets:match(?TWorking, {'$1', '_', CRef, '_'}) of
         [[Worker]] ->
             [{Worker, TRef, CRef, From}] = ets:take(?TWorking, Worker),
             shutdown_worker(Worker, brutal_kill),
@@ -243,7 +244,7 @@ handle_info(_Info, State) ->
     {noreply, State}.
 
 terminate(_Reason, State) ->
-    ok = lists:foreach(fun (W) -> shutdown_worker( W, State#state.shutdown) end, State#state.workers).
+    ok = lists:foreach(fun(W) -> shutdown_worker(W, State#state.shutdown) end, State#state.workers).
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
@@ -258,7 +259,7 @@ code_change(_OldVsn, State, _Extra) ->
 start_pool(StartFun, PoolArgs, WorkerArgs) ->
     case proplists:get_value(name, PoolArgs) of
         undefined ->
-            catch (exit ("pool name underfined"));
+            catch (exit("pool name undefined"));
         Name ->
             gen_server:StartFun(Name, ?MODULE, {PoolArgs, WorkerArgs}, [])
     end.
@@ -268,7 +269,7 @@ start_pool(StartFun, PoolArgs, WorkerArgs) ->
 give_back(Worker, true) when is_pid(Worker) ->
     case ets:first(?TWaiting) of
         '$end_of_table' ->
-            ets:insert_new(?TFree, {erlang:system_time(), Worker} );
+            ets:insert_new(?TFree, {erlang:system_time(), Worker});
         Key ->
             [{Key, TRef, WaitingClientRef, FromClient}] = ets:take(?TWaiting, Key),
             gen_server:reply(FromClient, Worker),
@@ -277,7 +278,7 @@ give_back(Worker, true) when is_pid(Worker) ->
 
 %% Disabled, IsEnable = false
 give_back(Worker, false) when is_pid(Worker) ->
-    ets:insert_new(?TFree, {erlang:system_time(), Worker} ).
+    ets:insert_new(?TFree, {erlang:system_time(), Worker}).
 
 
 try_get(Overflow, TRef, CRef, From) ->
@@ -301,17 +302,17 @@ init_ets(TableName) ->
     erlang:put(TableName, ets:new(TableName, [ordered_set, private])).
 
 
-launch_worker({M, F, A},  TRef, From ) ->
+launch_worker({M, F, A}, TRef, From) ->
     Launcher = async_call(M, F, A, new_worker),
-    ets:insert_new(?TLaunching, {Launcher, TRef, From }),
+    ets:insert_new(?TLaunching, {Launcher, TRef, From}),
     Launcher.
 
 async_call(M, F, A, Action) when is_atom(M), is_atom(F), is_list(A) ->
     SendTo = self(),
-    {Pid, _Ref} = spawn( fun() ->
-               R = call(M, F, A),
-               SendTo ! {Action, self(), R}
-           end),
+    {Pid, _Ref} = spawn(fun() ->
+        R = call(M, F, A),
+        SendTo ! {Action, self(), R}
+                        end),
     Pid.
 
 call(M, F, A) ->
@@ -324,7 +325,7 @@ call(M, F, A) ->
 prepopulate() ->
     Size = get(init_size),
     {M, F, A} = get(mfa),
-    lists:foreach(fun (_X) -> async_call(M, F, A, new_worker) end, lists:seq(1, Size)).
+    lists:foreach(fun(_X) -> async_call(M, F, A, new_worker) end, lists:seq(1, Size)).
 
 shutdown_worker(Pid, brutal_kill) ->
     case monitor_worker(Pid) of
@@ -360,14 +361,14 @@ shutdown_worker(Pid, Time) ->
     end.
 
 monitor_worker(Pid) ->
-erlang:monitor(process, Pid),
+    erlang:monitor(process, Pid),
     unlink(Pid),
     receive
         {'EXIT', Pid, Reason} ->
             receive
                 {'DOWN', _, process, Pid, _} ->
-                {error, Reason}
+                    {error, Reason}
             end
     after 0 ->
         ok
-end.
+    end.
